b7994c7f0d402f07c5dd6bbee6e6ef667e78068e
garff <garff@mayflower.work>
2024-03-20 17:54:24 +0200

    Add debug mode without gpt requests

--- components/helpers.py
+++ components/helpers.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
 
 from db import db_get_today_birthdays
+from settings.prod import DEBUG
 
 
 def get_wishes():
@@ -10,23 +11,32 @@ def get_wishes():
     for person in persons:
         text += f"имя - {person[1]}; отношения - {person[2]}; интересы - {person[4]}; пожелания - {person[5]};\n"
 
-    client = OpenAI()
+    if not DEBUG:
 
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system",
-             "content": """
-             Сгенерируйте поздравления с днем рождения для нескольких людей, учитывая их имена, отношения 
-             поздравляющего и именинника, интересы и специфические пожелания. Каждое поздравление должно быть кратким, 
-             в пределах 1-3 предложений. Поздравления должны быть разделены двойным символом ";;". Пожалуйста, 
-             учитывайте эти детали при составлении поздравлений.
-             """},
-            {"role": "user", "content": f"список людей для поздравления: \n {text}"}
-        ]
-    )
+        client = OpenAI()
 
-    response = completion.choices[0].message.content
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system",
+                 "content": """
+                 Сгенерируйте поздравления с днем рождения для нескольких людей, учитывая их имена, отношения 
+                 поздравляющего и именинника, интересы и специфические пожелания. Каждое поздравление должно быть кратким, 
+                 в пределах 1-3 предложений. Поздравления должны быть разделены двойным символом ";;". Пожалуйста, 
+                 учитывайте эти детали при составлении поздравлений.
+                 """},
+                {"role": "user", "content": f"список людей для поздравления: \n {text}"}
+            ]
+        )
+
+        response = completion.choices[0].message.content
+
+    else:
+        wishes = []
+        for person in persons:
+            wishes.append(f"Сгенерированное поздравление для {person[1]}")
+
+        response = ";;".join(wishes)
 
     return [{"chat_id": person[0], "name": person[1], "wish": wish} for person, wish in
             zip(persons, response.split(";;"))]
