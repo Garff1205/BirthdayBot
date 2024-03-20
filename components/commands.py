f931e320ada75ae2bee1e712ddd4a30814604c11
garff <garff@mayflower.work>
2024-03-20 15:39:43 +0200

    Implement add_birthday method

--- components/commands.py
+++ components/commands.py
@@ -1,6 +1,8 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
+from components.db import db_add_birthday
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
@@ -10,7 +12,18 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def add_birthday(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    pass
+    args = " ".join(context.args).split("; ")
+    if len(args) == 5:
+        db_add_birthday(update.message.chat_id, *args)
+    else:
+        await update.message.reply_text(
+            "Пожалуйста, используйте формат: "
+            "/add <имя именинника>; "
+            "<ваши с ним отношения (друг, сестра, дальний родственник>; "
+            "<дата рождения в формате YYYY-MM-DD>; "
+            "<интересы именника>; "
+            "<дополнительные пожелания к тексту поздравления>"
+        )
 
 
 async def check_all_birthdays(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
--- components/db.py
+++ components/db.py
@@ -11,10 +11,20 @@ CREATE_DB_QUERY = """CREATE TABLE IF NOT EXISTS birthdays
                     interests TEXT,
                     wishes TEXT)"""
 
+ADD_BIRTHDAY_QUERY = """INSERT INTO birthdays (chat_id, name, relative, birthdate, interests, wishes) VALUES (?, ?, ?, ?, ?, ?)"""
+
 
 def create_db():
     conn = sqlite3.connect(DB)
     c = conn.cursor()
     c.execute(CREATE_DB_QUERY)
+    conn.commit()
+    conn.close()
+
+
+def db_add_birthday(chat_id, name, relative, birthdate, interests, wishes):
+    conn = sqlite3.connect(DB)
+    c = conn.cursor()
+    c.execute(ADD_BIRTHDAY_QUERY, (chat_id, name, relative, birthdate, interests, wishes))
     conn.commit()
     conn.close()
