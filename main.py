db244c30b6678b9ea2e78fbd88b09cf3d575e200
garff <garff@mayflower.work>
2024-03-21 13:06:07 +0200

    Add Debug mode for cron

--- main.py
+++ main.py
@@ -3,19 +3,27 @@ from telegram.ext import Application, CommandHandler
 
 from components.commands import add_birthday, check_all_birthdays, start
 from components.functions import create_birthday_wish
-from settings.prod import TOKEN
+from settings.prod import TOKEN, DEBUG
 
 
 def main() -> None:
     application = Application.builder().token(TOKEN).build()
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(
-        func=create_birthday_wish,
-        trigger="cron",
-        hour=13,
-        minute=0,
-        kwargs={"bot": application.bot},
-    )
+    if not DEBUG:
+        scheduler.add_job(
+            func=create_birthday_wish,
+            trigger="cron",
+            hour=13,
+            minute=0,
+            kwargs={"bot": application.bot},
+        )
+    else:
+        scheduler.add_job(
+            func=create_birthday_wish,
+            trigger="cron",
+            second="*/10",
+            kwargs={"bot": application.bot},
+        )
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("add", add_birthday))
