5cc5264525c3769024b625dd5a5170b3e4e6d5c2
garff <garff@mayflower.work>
2024-03-27 17:58:09 +0200

    fix all birthdays query 2

--- components/db.py
+++ components/db.py
@@ -36,10 +36,10 @@ def db_add_birthday(chat_id, name, relative, birthdate, interests, wishes):
     conn.close()
 
 
-def db_get_all_birthdays(chat_id):
+def db_get_all_birthdays(chat_id: int):
     conn = sqlite3.connect(DB)
     c = conn.cursor()
-    c.execute(GET_ALL_BIRTHDAYS_QUERY, chat_id)
+    c.execute(GET_ALL_BIRTHDAYS_QUERY, str(chat_id))
     birthdays = c.fetchall()
     conn.close()
     return birthdays
